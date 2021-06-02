11421a1fc1a19f71ab2818bdd4e53e27a9f714bc
lizelive <github@lize.live>
2021-06-02 02:57:02 -0700

    works! need aprox one per large generator.

--- ExampleMod/ExampleMod/NuclearAllInOne.cs
+++ ExampleMod/ExampleMod/NuclearAllInOne.cs
@@ -1,6 +1,8 @@
 ﻿
+using BrilliantSkies.Blocks.SteamEngines.Ui;
 using BrilliantSkies.Core.Timing;
 using BrilliantSkies.Ftd.Constructs.Modules.Main.Power;
+using BrilliantSkies.Ftd.DamageLogging;
 using BrilliantSkies.Localisation;
 using BrilliantSkies.Localisation.Runtime.FileManagers.Files;
 using BrilliantSkies.Ui.Tips;
@@ -8,20 +10,23 @@ using System;
 
 namespace CultOfClang.NuclearReactor
 {
-    public class NuclearAllInOne : Block, ISteamProvider, IBlockWithSteamStats
+    public class NuclearAllInOne : SteamTank
     {
         public new static ILocFile _locFile = Loc.GetFile("NuclearAllInOne");
         const float MultiplyerPowerDensity = 100; // normal is 15 real can do 100mw/m^3
         const float HeatPerVolume = 40;
 
-        public float EnergyPerSecond => MultiplyerPowerDensity * (float)this.item.SizeInfo.ArrayPositionsUsed;
+        public float RtgVolume => MultiplyerPowerDensity * (float)this.item.SizeInfo.ArrayPositionsUsed;
+        public float SteamPerSecond => 1000 * (float)this.item.SizeInfo.ArrayPositionsUsed;
 
         protected override void AppendToolTip(ProTip tip)
         {
             base.AppendToolTip(tip);
-            tip.SetSpecial_Name(RTG._locFile.Get("SpecialName", "Radioisotope Thermoelectric Generator (RTG)", true), RTG._locFile.Get("SpecialDescription", "Nuclar Boiler generates endless steam using the power of the atom", true));
+            tip.SetSpecial_Name(RTG._locFile.Get("SpecialName", "Simple Reactor", true), RTG._locFile.Get("SpecialDescription", "Nuclar Boiler generates endless steam using the power of the atom", true));
             tip.InfoOnly = true;
-            tip.Add(Position.Middle, RTG._locFile.Format("Return_CreatesEnergyPer", "Creates <<{0} steam per second>>", EnergyPerSecond));
+            tip.Add(Position.Middle, RTG._locFile.Format("Return_CreatesEnergyPer", "Creates <<{0} steam per second>>", SteamPerSecond));
+            if (this.StorageModule != null)
+                SteamSharedUiHelper.AppendPressureAndCapacityBar(tip, this.StorageModule);
         }
 
         public override void StateChanged(IBlockStateChange change)
@@ -29,89 +34,43 @@ namespace CultOfClang.NuclearReactor
             base.StateChanged(change);
             if (change.IsAvailableToConstruct)
             {
-                this.MainConstruct.PowerUsageCreationAndFuelRestricted.RtgVolume += EnergyPerSecond;
+                this.MainConstruct.PowerUsageCreationAndFuelRestricted.RtgVolume += RtgVolume;
                 this.MainConstruct.HotObjectsRestricted.AddASimpleSourceOfBodyHeat(HeatChange);
+                this.MainConstruct.SchedulerRestricted.RegisterFor1PerSecond(Update);
+
             }
             else
             {
                 if (!change.IsLostToConstructOrConstructLost)
                     return;
-                this.MainConstruct.PowerUsageCreationAndFuelRestricted.RtgVolume -= EnergyPerSecond;
+                this.MainConstruct.PowerUsageCreationAndFuelRestricted.RtgVolume -= RtgVolume;
                 this.MainConstruct.HotObjectsRestricted.RemoveASimpleSourceofBodyHeat(HeatChange);
+                this.MainConstruct.SchedulerRestricted.UnregisterFor1PerSecond(Update);
+
             }
         }
 
         private float HeatChange => (float)(this.item.SizeInfo.ArrayPositionsUsed * HeatPerVolume);
-
-        public SteamPartType PartType => SteamPartType.Boiler;
-
-        public float Pressure => 1000 * Amount / Capacity;
-
-        public float Capacity => this.item.SizeInfo.ArrayPositionsUsed * 10;
-
-        public float Amount { get; set; }
-
-        public ulong LastFlowFrame { get; set; }
-        SteamStats IBlockWithSteamStats.Stats { get => new SteamStats(10)
-        {
-            
-        }; set => throw new NotImplementedException(); }
-
        
 
         public override BlockTechInfo GetTechInfo()
         {
             return new BlockTechInfo()
-                .AddSpec(RTG._locFile.Get("TechInfo_EnergyGeneratedPer", "Energy generated per second", true), EnergyPerSecond)
-                .AddSpec("Steam", Amount)
+                .AddSpec(RTG._locFile.Get("TechInfo_EnergyGeneratedPer", "Energy generated per second", true), RtgVolume)
+                .AddSpec("Steam", this.StorageModule?.Amount)
                 .AddStatement(RTG._locFile.Format("TechInfo_IrDetectionRange", "Adds {0}{1} to body temperature of the vehicle. This will have a small impact on IR detection range from all angles.",
     this.HeatChange,
      "°C/m\u00B3"
 ));
         }
 
-        public override InteractionReturn Secondary()
-        {
-            return new InteractionReturn($"i have {Amount} steam");
-        }
-
-        public float TakeSteam(float amountToTake)
-        {
-            float num = Math.Min(this.Amount, amountToTake);
-            this.Amount -= amountToTake;
-            return num;
-        }
-
-        public override void BlockStart()
-        {
-            this.MainConstruct.SchedulerRestricted.RegisterForHalfPerSecond(Update);
-          }
-
-        public override void ScrapBlock()
-        {
-            this.MainConstruct.SchedulerRestricted.UnregisterForHalfPerSecond(Update);
-        }
-
-        const float RegenRate = 100;
         private void Update(ISectorTimeStep obj)
         {
-            Amount += obj.DeltaTime * RegenRate;
-        }
-
-        /*
-         base.StateChanged(change);
-            if (change.InitiatedOrInitiatedInUnrepairedState_OnlyCalledOnce)
-            {
-                this.MainConstruct.SchedulerRestricted.RegisterForFixedUpdate(new Action<ITimeStep>(this.FixedStep));
-            }
-            else if (change.IsPermanentlyRemovedOrConstructDestroyed)
-            {
-                this.MainConstruct.SchedulerRestricted.UnregisterForFixedUpdate(new Action<ITimeStep>(this.FixedStep));
-            }*/
-
-        public void AttachStats(SteamStats change)
-        {
-            change.AddBlock(this);
+            var steam = obj.DeltaTime * SteamPerSecond;
+            this.StorageModule.AddSteam(steam);
+            this.Stats.BoilerSteamCreated.Add(steam);
+            if(StorageModule.Pressure > 9)
+                this.ExplodeBlock(new BaseDamageLogger());
         }
     }
 }
