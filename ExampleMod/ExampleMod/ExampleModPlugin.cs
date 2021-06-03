d5371c38760fba734b0a258ef61d7f73995abf26
lizelive <github@lize.live>
2021-06-03 00:25:08 -0700

    fun

--- ExampleMod/ExampleMod/ExampleModPlugin.cs
+++ ExampleMod/ExampleMod/ExampleModPlugin.cs
@@ -15,22 +15,22 @@ namespace CultOfClang.NuclearReactor
     /// All code files using the GamePlugin or GamePlugin_PostLoad interfaces (no need to use both)
     /// will have their OnLoad method called when they are loaded by the plugin loader.
     /// </summary>
-    public class ExampleModPlugin : GamePlugin , GamePlugin_PostLoad
+    public class ExampleModPlugin : GamePlugin
     {
 
 
-        public void OnLoad()
-        {
-            AdvLogger.LogInfo("Loaded Example Mod");
-        }
+        //public void OnLoad()
+        //{
+        //    AdvLogger.LogInfo("Loaded Example Mod");
+        //}
 
-        /// <summary>
-        /// Not currently called from anywhere in FTD.
-        /// </summary>
-        public void OnSave()
-        {
-            AdvLogger.LogInfo("Saved Example Mod");
-        }
+        ///// <summary>
+        ///// Not currently called from anywhere in FTD.
+        ///// </summary>
+        //public void OnSave()
+        //{
+        //    AdvLogger.LogInfo("Saved Example Mod");
+        //}
 
 
         /// <summary>
@@ -38,7 +38,7 @@ namespace CultOfClang.NuclearReactor
         /// </summary>
         public string name
         {
-            get { return "ExampleMod"; }
+            get { return "Nuclear Reactor"; }
         }
 
         /// <summary>
@@ -50,56 +50,56 @@ namespace CultOfClang.NuclearReactor
         }
 
 
-        /// <summary>
-        /// Used if using GamePlugin_PostLoad interface.
-        /// </summary>
-        /// <returns></returns>
-        public bool AfterAllPluginsLoaded()
-        {
-            AdvLogger.LogInfo("Called after all other mods loaded... Example Mod");
-            ModdingEvents.AddYourModules += HookUp;
-            return true;
-        }
+        ///// <summary>
+        ///// Used if using GamePlugin_PostLoad interface.
+        ///// </summary>
+        ///// <returns></returns>
+        //public bool AfterAllPluginsLoaded()
+        //{
+        //    AdvLogger.LogInfo("Called after all other mods loaded... Example Mod");
+        //    ModdingEvents.AddYourModules += HookUp;
+        //    return true;
+        //}
 
-        private void HookUp(IDictionaryOfTypedTypes<IComponentContainer> typecontainer, string directory)
-        {
-            // Add our ModWidgets container to all mods / configurations from now on.
-            //typecontainer.Add(new ModWidgets(directory));
-        }
+        //private void HookUp(IDictionaryOfTypedTypes<IComponentContainer> typecontainer, string directory)
+        //{
+        //    // Add our ModWidgets container to all mods / configurations from now on.
+        //    typecontainer.Add(new ModWidgets(directory));
+        //}
 
 
     }
 
     
 
-    public class ModWidgets : AbstractContainer<ModWidget>
-    {
-        public ModWidgets(string directoryWithTrailingSlash) : base(directoryWithTrailingSlash)
-        {
-            CreateUiContent("Widget example", "Added by ExampleMod. Used to demonstrate how to add your own categories of object via mods");
-        }
+    //public class ModWidgets : AbstractContainer<ModWidget>
+    //{
+    //    public ModWidgets(string directoryWithTrailingSlash) : base(directoryWithTrailingSlash)
+    //    {
+    //        CreateUiContent("Widget example", "Added by ExampleMod. Used to demonstrate how to add your own categories of object via mods");
+    //    }
 
-        public override IModComponent AddNew()
-        {
-            var newOne = new ModWidget();
-            Components.Add(newOne);
-            return newOne;
-        }
+    //    public override IModComponent AddNew()
+    //    {
+    //        var newOne = new ModWidget();
+    //        Components.Add(newOne);
+    //        return newOne;
+    //    }
 
-        protected override string DirName => "MyWidgets";
+    //    protected override string DirName => "MyWidgets";
 
-        protected override string FileExtension => ".myWidget";
-    }
+    //    protected override string FileExtension => ".myWidget";
+    //}
 
-    public class ModWidget : ModComponentAbstract
-    {
-        public string OurUselessString { get; set; } = "Useless string";
+    //public class ModWidget : ModComponentAbstract
+    //{
+    //    public string OurUselessString { get; set; } = "Useless string";
 
-        public override void GuiEditor()
-        {
-            base.GuiEditor();
-           OurUselessString =  MapEditorGuiCommon.StringEditor("Useless string example", OurUselessString, new ToolTip("Just an example"));
-        }
-    }
+    //    public override void GuiEditor()
+    //    {
+    //        base.GuiEditor();
+    //       OurUselessString =  MapEditorGuiCommon.StringEditor("Useless string example", OurUselessString, new ToolTip("Just an example"));
+    //    }
+    //}
 
 }
